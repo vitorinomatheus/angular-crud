811dab7dd3fee214aa917f3c9277b1dfe1c245c4
vitorinomatheus <matheusvs122@gmail.com>
2022-07-14 11:35:06 -0300

    change header's styling method

--- src/app/app-header/app-header.component.ts
+++ src/app/app-header/app-header.component.ts
@@ -9,26 +9,4 @@ export class AppHeaderComponent {
 
   appTitle = 'myLibrary';
 
-  headerStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    fontFamily: "'Stint Ultra Condensed', 'sans-serif'",
-    padding: '1rem 2rem 1rem 2rem',
-    fontSize: '2.5rem',
-    boxShadow: '1px 1px 4px black'
-  }
-
-  titleStyle = {
-    margin: '0',
-    cursor: 'pointer'
-  }
-  
-  buttonStyle = {
-    border: 'none',
-    backgroundColor: 'white',
-    fontFamily: 'Roboto',
-    fontSize: '1.7rem',
-    cursor: 'pointer'
-  }
-
 }
